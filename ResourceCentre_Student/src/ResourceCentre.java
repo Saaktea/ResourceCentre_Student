28d1e8fcc5068c2c003dd683a59ac836b5795e55
21008035 <21008035@DESKTOP-TM69ORM>
2022-07-13 15:57:43 +0800

    Commit by Razi

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -294,7 +294,6 @@ public class ResourceCentre {
 				chromebookList.get(i).setIsAvailable(true);
 				chromebookList.get(i).setDueDate("");
 				isReturned = true;
-				
 			}
 		}
 		return isReturned;
